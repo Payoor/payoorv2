7b543a0cbc11d7120254d80c528cd8657679c92a
Samson Adebayo <sampelumibiz@gmail.com>
2026-06-28 19:16:27 +0100

    iOS config

--- payoor_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ payoor_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_appauth
 import flutter_secure_storage_darwin
 import geolocator_apple
 import sqflite_darwin
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
